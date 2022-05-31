7fcc67ae569489b79d7b69be855d9abd4bea6ba9
= <kelly.p.jardim@gmail.com>
2022-05-31 06:42:04 -0300

    Primeiro commit do projeto

---
+++ src/br/com/glandata/Casting1.java
@@ -0,0 +1,12 @@
+package br.com.glandata;
+
+public class Casting1 {
+
+	public static void main(String[] args) {
+		long numeroInteiro = 1024;
+		int numeroLongo = (int) numeroInteiro;
+		System.out.println(numeroInteiro);
+
+	}
+
+}
